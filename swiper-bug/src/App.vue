272b3f93733dfe7fb71e39dffef13b4367e7c213
Angel Recinos <angel.recinos@coalitiontechnologies.com>
2022-01-26 14:23:40 -0600

    vite package resolution bug.

---
+++ swiper-bug/src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <swiper
+    :slides-per-view="1"
+    :space-between="50"
+  >
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    ...
+  </swiper> 
+  
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
